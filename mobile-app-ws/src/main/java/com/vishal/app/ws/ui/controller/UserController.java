7962fbf5d802bb15f875fa1d4b8f78b51558843a
vishal <vishalambekar459@gmail.com>
2021-06-23 13:37:24 +0530

    done till query and URI

--- mobile-app-ws/src/main/java/com/vishal/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/vishal/app/ws/ui/controller/UserController.java
@@ -6,14 +6,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")  // http://localhost:8080/users
 public class UserController {
 	
+	@GetMapping  
+	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "10") int limit,
+			@RequestParam(value = "sort",required = false) String sort
+			)                  //requestparam is used to get query(after ?) parameters which needs to passed in post man ------->  (http://localhost:8080/users?page=94&limit=7)
+	{
+		return "get user was called with page --> "+page+"  and limit --> "+limit+"    and sort --> "+sort;
+	}
+	
 	@GetMapping(path = "/{userId}")   // http://localhost:8080/users/{userId}
-	public String getUser(@PathVariable String userId)
+	public String getUser(@PathVariable String userId)                               //path variable is used to get uri paramenter
 	{
 		return "get user was called with user Id :: "+userId;
 	}
